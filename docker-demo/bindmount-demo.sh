d20b597c2bc40823721ad88ee65f671f66ea6640
Ruben Orduz <rubenoz@gmail.com>
2018-07-22 23:23:17 +0100

    demoized docker stuff

--- docker-demo/bindmount-demo.sh
+++ docker-demo/bindmount-demo.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
+
+# import demo 
+. ../demo-magic.sh -c
+
 # USING A BIND MOUNT
 
-mkdir /tmp/host
-echo 'Hi from a bind mount' > /tmp/host/file.txt
+pe 'mkdir /tmp/host'
+pe 'echo "Hi from a bind mount" > /tmp/host/file.txt'
 # Run a container with the /tmp/host directory assigned to '/container' in the container.
-docker run --detach -ti --name bindtest -v /tmp/host:/container busybox
+pe 'docker run --detach -ti --name bindtest -v /tmp/host:/container busybox'
 
 # Execute a command to look at the contents within the container
-docker exec bindtest cat /container/file.txt
+pe 'docker exec bindtest cat /container/file.txt'
 
 # Look at the information Docker knows about the mounts for the container
-docker inspect bindtest | jq '.[].Mounts'
+pe "docker inspect bindtest | jq '.[].Mounts'"
 
 # Clean up
-docker kill bindtest
-docker rm bindtest
+pe 'docker kill bindtest'
+pe 'docker rm bindtest'
 rm -rf /tmp/host
--- docker-demo/volume-demo.sh
+++ docker-demo/volume-demo.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+# import demo
+. ../demo-magic.sh -c
 # USING A VOLUME
 
 # List the current containers
-docker volume ls
+pe 'docker volume ls'
 # Make a volume
-docker volume create myvol
+pe 'docker volume create myvol'
 # Make some example data
-echo 'Hello from a volume' > /tmp/file.txt
+pe "echo 'Hello from a volume' > /tmp/file.txt"
 # Attach to a container
-docker run --tty --detach --mount source=myvol,destination=/data --name voltest busybox
+pe 'docker run --tty --detach --mount source=myvol,destination=/data --name voltest busybox'
 # Copy our data in
-docker cp /tmp/file.txt voltest:/data
+pe 'docker cp /tmp/file.txt voltest:/data'
 # Execute a command in the container to verify that data
-docker exec voltest cat /data/file.txt
+pe 'docker exec voltest cat /data/file.txt'
 
 # Clean up
 docker container kill voltest
